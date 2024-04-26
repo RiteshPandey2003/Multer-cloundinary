5605fe6783df0ec5d4e73cd223f3056d6086888e
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-04-26 08:34:04 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import express from "express";
+import cookieParser from "cookie-parser";
+import productRoute from "./routes/product.js";
+import userRoute from "./routes/user.js";
+import { corsOption } from "./constant/config.js";
+import cors from "cors";
+
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
+const app = express();
 
+app.use(cors(corsOption))
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use("/cm", userRoute, productRoute);
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
-
-
-
-
-
-
-
-
-
+  });
